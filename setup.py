df67f183d0a44a8bccbde6d912c6b426b735b62b
Juliano Martinez <juliano@falaserio.org>
2011-08-21 00:09:38 -0300

    updated read-me

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup
 
 setup(name='otto',
       version='0.0.1',
-      description='A simple way to use redis as backend for python ConfigParser',
+      description='S3 Clone on top of cyclone'
       author='Juliano Martinez',
       author_email='juliano@martinez.io',
       url='https://github.com/ncode/otto'
-      install_requires=['redis','simplejson'],
-      packages=['RedisConfigParser'],
+      install_requires=['cyclone','txriak'],
+      packages=['otto'],
      )
